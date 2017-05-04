d7c569600627e489f9e2ce45f30fc181a5cf7ad0
Ardy Kamdani <kamdani.af@gmail.com>
2017-05-03 21:31:32 -0500

    added function to add new tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
+import { FormGroup, InputGroup, FormControl, Button } from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      noteArray: [],
+    };
+    this.handleAdd = this.handleAdd.bind(this);
+  }
+  handleAdd(event) {
+    event.preventDefault();
+    this.setState({
+      noteArray: this.state.noteArray.concat(this.textInput.value),
+    });
+  }
   render() {
+    const noteComponents = this.state.noteArray.map(note => (<div>{note}</div>));
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>{'Welcome to Ardy\'s Todo App'}</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {noteComponents}
+        <form onSubmit={this.handleAdd}>
+          <FormGroup>
+            <InputGroup>
+              <InputGroup.Addon>
+                <input type="checkbox" aria-label="..." />
+              </InputGroup.Addon>
+              <FormControl inputRef={(input) => { this.textInput = input; }} type="text" />
+            </InputGroup>
+          </FormGroup>
+          <Button type="submit" onClick={this.handleAdd}>Hello</Button>
+        </form>
       </div>
     );
   }
